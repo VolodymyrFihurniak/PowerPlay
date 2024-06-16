8b637fdc0e0f2e4d3f0c3a8d961fd0cbb64b9bf6
Volodymyr Fihurniak <volodymyr.fihurniak@gmail.com>
2024-06-16 18:25:35 +0000

    Initialize the project via the bun package manager and configure the environment

---
+++ server/eslint.config.mjs
@@ -0,0 +1,39 @@
+import eslint from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default tseslint.config(
+  {
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      prettier: prettierPlugin,
+    },
+  },
+  {
+    ignores: ['dist', 'node_modules', 'coverage', 'eslint.config.js'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.es2020,
+      },
+      parserOptions: {
+        project: ['tsconfig.json', 'tsconfig.node.json'],
+      },
+    },
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    rules: {
+      ...prettierPlugin.configs.recommended.rules,
+      ...eslintConfigPrettier.rules,
+      'max-lines': ['warn', { max: 124 }],
+    },
+  }
+);
